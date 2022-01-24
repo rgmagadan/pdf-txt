1691e8e4a424fac811f0d71058ae93f8f3f96296
rgmagadan <rgmagadan@yahoo.es>
2022-01-24 12:49:46 +0100

    Añade todos los archivos a un solo txt y borra los parciales.

--- pdf-texto.ps1
+++ pdf-texto.ps1
@@ -1,4 +1,5 @@
-
+Write-Host "Trabajando..."
+$contador = 0
 $word = New-Object -comobject Word.Application
 $Format = [Microsoft.Office.Interop.Word.WdSaveFormat]::wdFormatUnicodeText
 foreach( $item in Get-ChildItem $args[0])
@@ -6,5 +7,10 @@ foreach( $item in Get-ChildItem $args[0])
 $doc = $word.Documents.Open($item.fullname)
 $doc.SaveAs($item.fullname+".txt", $format)
 $doc.Close()
+$nombre = $item.fullname+".txt"
+Get-Content $nombre | Add-Content file.txt -Encoding UTF8
+Remove-Item $nombre
+$contador++
 }
 $word.Quit()
+Write-Host ("Listo. Se han convertido " + $contador.ToString() + " archivos.")
